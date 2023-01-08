10acd7fe89dee8b414a556b3232e0b05b4c02eec
algorithmgit <48445424+algorithmgit@users.noreply.github.com>
2023-01-08 17:20:32 -0500

    Update issues_with_thread_&_locks.c
    Done, Count = 1043605767
    Elapsed time: 11.000000 seconds

--- issues_with_thread_&_locks.c
+++ issues_with_thread_&_locks.c
@@ -1,5 +1,8 @@
 /* 
-   These set of examples starts with counting to a billion by two function calls. On my machine this takes about 4 seconds.
+   Updated the previous commit to redo the counting using threads. To see if this speeds up the progream. As we can see in the output, it did not. 
+   Also, the count is incorrect. 
+   In the next commit we will fix this issue via multex etc. This code should be read from the first commit to later commits in sequence to understand 
+   how we are improving the issues we see in the code. 
 */
 
 #include <stdio.h>
@@ -12,7 +15,7 @@
 #define BIG_COUNT 1000000000UL
 uint32_t count = 0; 
 
-void count_to_big () {
+void *count_to_big (void *var) {
     for(uint32_t i = 0; i < BIG_COUNT; i++){
         count++;
     }
@@ -21,10 +24,14 @@ void count_to_big () {
 int main(){
 
     time_t start_time, end_time;
-
+    pthread_t tid1; 
+    pthread_t tid2;
+    
     start_time = time( NULL );
-    count_to_big();
-    count_to_big();
+    pthread_create(&tid1, NULL, count_to_big, NULL);
+    pthread_create(&tid2, NULL, count_to_big, NULL);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
     end_time = time( NULL );
     
     printf("Done, Count = %u\n", count);
