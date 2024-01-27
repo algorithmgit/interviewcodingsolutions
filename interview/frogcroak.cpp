e892929087b8b9a48bbd2dc947bb8fa024f79496
algorithmgit <48445424+algorithmgit@users.noreply.github.com>
2024-01-27 11:30:18 -0500

    Create frogcroak.cpp
    Output : 
    
    frogs = 2
    frogs = 2
    frogs = -1

---
+++ interview/frogcroak.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int minNumberOfFrogs(string croakOfFrogs) {
+    int cnt[5] = {}, frogs = 0, max_frogs = 0;
+    for (auto ch : croakOfFrogs) {
+        auto n = string("croak").find(ch);
+        ++cnt[n];
+        if (n == 0){
+            max_frogs++;++frogs;// = max(++max_frogs, ++frogs);
+        }
+        else if (--cnt[n - 1] < 0)
+            return -1;
+        else if (n == 4)
+            --frogs;
+    }
+    return frogs == 0 ? max_frogs : -1;
+}
+
+int main()
+{
+    printf("frogs = %d\n", minNumberOfFrogs("croakcroak"));
+    printf("frogs = %d\n", minNumberOfFrogs("crcoakroak"));
+    printf("frogs = %d\n", minNumberOfFrogs("croakcrook"));
+
+    return 0;
+}
+
+
+/* Output : 
+frogs = 2
+frogs = 2
+frogs = -1
+*/
